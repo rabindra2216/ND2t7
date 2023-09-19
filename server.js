acdce4c07fd1fe178f034d23fb55145919b7ea71
Rabindra <rabindrayadav2216@gmail.com>
2023-09-20 02:25:15 +0530

    Intial Commit

--- server.js
+++ server.js
@@ -121,56 +121,56 @@ app.delete("/employees/:empCode",function(req,res){
 });
 
 // postsql
-const { Client } = require("pg");
- const client = new Client({
-    user: "postgres",
-     password: "Rdatabase@2216",
-     database:"postgres",
-    port: 5432,
-    host: "db.aldnuxtgfiamykwzhnxv.supabase.co", 
-    ssl: { rejectUnauthorized: false },
-    }); 
-    client.connect(function (res, error)
-     { console.log("Connected!!!");
+// const { Client } = require("pg");
+//  const client = new Client({
+//     user: "postgres",
+//      password: "Rdatabase@2216",
+//      database:"postgres",
+//     port: 5432,
+//     host: "db.aldnuxtgfiamykwzhnxv.supabase.co", 
+//     ssl: { rejectUnauthorized: false },
+//     }); 
+//     client.connect(function (res, error)
+//      { console.log("Connected!!!");
     
-    });
+//     });
     
-    app.get("/users", function (req, res, next) 
-    { console.log("Inside /users get api");
-    const query = `SELECT * FROM users`;
-client.query(query, function (err, result)
- { if (err) 
-{ res.status(400).send(err);}
-res.send(result.rows);
-client.end();
-});
-});
+//     app.get("/users", function (req, res, next) 
+//     { console.log("Inside /users get api");
+//     const query = `SELECT * FROM users`;
+// client.query(query, function (err, result)
+//  { if (err) 
+// { res.status(400).send(err);}
+// res.send(result.rows);
+// client.end();
+// });
+// });
 
-app.post("/user", function (req, res, next)
- { console.log("Inside post of user"); 
- var values = Object.values (req.body);
-console.log(values);
-const query =`INSERT INTO users (email, firstname, lastname, age) VALUES ($1,$2,$3,$4)`;
-client.query(query, values, function (err, result) {
-    if (err) {
-    res.status(400).send(err);
-    }
-    //console.log(result);
-    res.send(`${result.rowCount} insertion successful`);
-    });
+// app.post("/user", function (req, res, next)
+//  { console.log("Inside post of user"); 
+//  var values = Object.values (req.body);
+// console.log(values);
+// const query =`INSERT INTO users (email, firstname, lastname, age) VALUES ($1,$2,$3,$4)`;
+// client.query(query, values, function (err, result) {
+//     if (err) {
+//     res.status(400).send(err);
+//     }
+//     //console.log(result);
+//     res.send(`${result.rowCount} insertion successful`);
+//     });
     
-    });
+//     });
 
-    //put
-    app.put("/user/:id", function (req, res, next) 
-    { console.log("Inside put of use r^ prime prime ") ;
-    let userId = req.params.id;
-   let age = req.body.age;
-  let values= [age,userId]
-const query =`UPDATE users SET age =$1 WHERE id= $2`;
-client.query(query, values, function (err, result)
- { if (err) {res.status(400).send(err);}
+//     //put
+//     app.put("/user/:id", function (req, res, next) 
+//     { console.log("Inside put of use r^ prime prime ") ;
+//     let userId = req.params.id;
+//    let age = req.body.age;
+//   let values= [age,userId]
+// const query =`UPDATE users SET age =$1 WHERE id= $2`;
+// client.query(query, values, function (err, result)
+//  { if (err) {res.status(400).send(err);}
 
-res.send(`${result.rowCount} updation successful`);
-});
-});
+// res.send(`${result.rowCount} updation successful`);
+// });
+// });
